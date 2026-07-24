ea96f7fed29967d75b2c189df8cefd403dea5a7c
Sairaj popat Londhe <sairajlondhe683@gmail.com>
2026-07-24 18:14:07 +0530

    Update EKS module version and configuration

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -1,50 +1,76 @@
 module "eks" {
+
   source  = "terraform-aws-modules/eks/aws"
-  version = "~> 21.0"
+  version = "19.15.1"
 
-  # v21 parameter names
-  name               = local.name
-  kubernetes_version = "1.33" # or your desired version
+  cluster_name                   = local.name
+  cluster_endpoint_public_access = true
 
-  endpoint_public_access  = true
-  endpoint_private_access = true
-
-  vpc_id     = module.vpc.vpc_id
-  subnet_ids = module.vpc.public_subnets
-
-  enable_cluster_creator_admin_permissions = true
-
-  addons = {
+  cluster_addons = {
     coredns = {
       most_recent = true
     }
-
     kube-proxy = {
       most_recent = true
     }
-
     vpc-cni = {
       most_recent = true
     }
   }
 
-  eks_managed_node_groups = {
-    tws-demo-ng = {
-      instance_types = ["t3.large"]
+  vpc_id                   = module.vpc.vpc_id
+  subnet_ids               = module.vpc.public_subnets
+  control_plane_subnet_ids = module.vpc.intra_subnets
 
+  # EKS Managed Node Group(s)
+
+  eks_managed_node_group_defaults = {
+
+    instance_types = ["t2.large"]
+
+    attach_cluster_primary_security_group = true
+
+  }
+
+
+  eks_managed_node_groups = {
+
+    tws-demo-ng = {
       min_size     = 2
       max_size     = 3
       desired_size = 2
 
-      capacity_type = "ON_DEMAND"
+      instance_types = ["t2.large"]
+      capacity_type  = "SPOT"
 
-      disk_size = 35
+      disk_size = 35 
+      use_custom_launch_template = false  # Important to apply disk size!
 
       tags = {
-        Name        = "tws-demo-ng"
+        Name = "tws-demo-ng"
         Environment = "dev"
-        ExtraTag    = "e-commerce-app"
+        ExtraTag = "e-commerce-app"
       }
+    }
+  }
+ 
+  tags = local.tags
+
+
+}
+
+data "aws_instances" "eks_nodes" {
+  instance_tags = {
+    "eks:cluster-name" = module.eks.cluster_name
+  }
+
+  filter {
+    name   = "instance-state-name"
+    values = ["running"]
+  }
+
+  depends_on = [module.eks]
+}
     }
   }
 
