132e61d508d6df3ab542739cb6c1cd7354d75634
Sairaj popat Londhe <sairajlondhe683@gmail.com>
2026-07-24 18:28:16 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.100"
+    }
+  }
+}
 locals {
 
   region          = "eu-west-1"
