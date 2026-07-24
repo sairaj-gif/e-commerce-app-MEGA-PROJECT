8e289b3ae84f669cd3ad48633f4f9595a9c598ce
Sairaj popat Londhe <sairajlondhe683@gmail.com>
2026-07-24 19:46:48 +0530

    Update install_tools.sh

--- terraform/install_tools.sh
+++ terraform/install_tools.sh
@@ -5,16 +5,21 @@ sudo apt update
 sudo apt install -y fontconfig openjdk-17-jre 
 
 # Jenkins installation
-sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
-  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
-echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
-  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
-  /etc/apt/sources.list.d/jenkins.list > /dev/null
-sudo apt-get update
-sudo apt-get -y install jenkins
+sudo apt-get install -y curl gnupg ca-certificates
 
-sudo systemctl start jenkins
+sudo mkdir -p /etc/apt/keyrings
+
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2026.key | \
+sudo tee /etc/apt/keyrings/jenkins-keyring.asc > /dev/null
+
+echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/" | \
+sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+
+sudo apt-get update
+sudo apt-get install -y jenkins
+
 sudo systemctl enable jenkins
+sudo systemctl start jenkins
 
 # Docker installation
 sudo apt-get update
